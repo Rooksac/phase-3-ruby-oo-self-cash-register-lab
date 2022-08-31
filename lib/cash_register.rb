67ec79eea27322f833bfd6924be5c74dfc073d34
Zachary Dean <Zakedoubled@gmail.com>
2022-08-31 11:39:24 -0400

    commit tree 3d3a9b3260d0fba0d263e6e805af40e524afd1e8

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total, :items, :last_transaction
 
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+        @last_transaction = nil
+    end
+
+    def add_item (title, price, quantity = 1)
+        self.total += (price * quantity)
+        for items in 1..quantity do
+        @items.push(title)
+        end
+        self.last_transaction = price*quantity
+    end
+
+    def apply_discount
+        if @discount != 0
+            self.total -= (total*(discount/100.to_f))
+            "After the discount, the total comes to $#{total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total -= @last_transaction
+    end 
+
+end
